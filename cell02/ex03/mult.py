9381490057d34a32f3ac34736b8d116b957a92a0
Pongsapak68040334 <Poolsub16@gmail.com>
2025-07-21 10:25:44 +0000

    mult

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+num1 = 5
+num2 = -3
+print(f"{num1} x {num2} = {num1 * num2}")
+if num1 * num2 > 0:
+    print("The result is positive.")
+elif num1 * num2 < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
